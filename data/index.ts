aa17deed215ed67374cdee23b4aebc7b9d9b0563
Tom Charles <thomas.charles23@gmail.com>
2023-10-06 13:21:38 -0600

    add october schedule

--- data/index.ts
+++ data/index.ts
@@ -1,190 +1,189 @@
 import { CalendarItem } from '@/utils/types'
 
-export const data: CalendarItem[] = [
-  {
-    day: 'Sunday',
-    date: 'Oct 1st',
-    name: 'Tacos Al Sabor',
-    url: ''
-  },
-  {
-    day: 'Monday',
-    date: 'Oct 2nd',
-    name: 'Rolling Italian & HipPOPs',
-    url: 'https://rollingitalianonline.square.site'
-  },
-  {
-    day: 'Tuesday',
-    date: 'Oct 3rd',
-    name: 'Revolver',
-    url: 'https://www.revolverfoodtruck.com/menus'
-  },
-  {
-    day: 'Wednesday',
-    date: 'Oct 4th',
-    name: ' Samos Grill',
-    url: 'https://roaminghunger.com/samos-grill/'
-  },
-  {
-    day: 'Thursday',
-    date: 'Oct 5th',
-    name: 'Woodhill Small Batch BBQ',
-    url: 'http://woodhillsmallbatchbbq.com/'
-  },
-  {
-    day: 'Friday',
-    date: 'Oct 6th',
-    name: 'Los Dos',
-    url: 'http://www.los2potrillos.com/'
-  },
-  {
-    day: 'Saturday',
-    date: 'Oct 7th',
-    name: 'Los Chamacos',
-    url: 'https://www.facebook.com/people/El-chamacos-taco-dealer-my-legacy/100057675699460/'
-  },
-  {
-    day: 'Sunday',
-    date: 'Oct 8th',
-    name: 'No Food Truck',
-    url: ''
-  },
-  {
-    day: 'Monday',
-    date: 'Oct 9th',
-    name: 'No Food Truck',
-    url: ''
-  },
-  {
-    day: 'Tuesday',
-    date: 'Oct 10th',
-    name: 'Rag Bag BIstro',
-    url: ''
-  },
-  {
-    day: 'Wednesday',
-    date: 'Oct 11th',
-    name: 'Arepa’s Caribbean Food & Simply Le Crepes',
-    url: ''
-  },
-  {
-    day: 'Thursday',
-    date: 'Oct 12th',
-    name: 'PinkTank',
-    url: 'https://www.eatpinkgettanked.com/'
-  },
-  {
-    day: 'Friday',
-    date: 'Oct 13th',
-    name: 'Schnitzelwirts ',
-    url: ''
-  },
-  {
-    day: 'Saturday',
-    date: 'Oct 14th',
-    name: ' Originals by Greeks ',
-    url: ''
-  },
-  {
-    day: 'Sunday',
-    date: 'Oct 15th',
-    name: 'MIke’s Coneys (3pm - 8 pm)',
-    url: ''
-  },
-  {
-    day: 'Monday',
-    date: 'Oct 16th',
-    name: 'Seasoned Swine & HipPops',
-    url: ''
-  },
-  {
-    day: 'Tuesday',
-    date: 'Oct 17th',
-    name: 'Sizzle',
-    url: 'https://www.sizzlefoodtruck.com/menu.html'
-  },
-  {
-    day: 'Wednesday',
-    date: 'Oct 18th',
-    name: 'Isan Thai',
-    url: ''
-  },
-  {
-    day: 'Thursday',
-    date: 'Oct 19th',
-    name: 'Woodhill Small Batch BBQ',
-    url: 'http://woodhillsmallbatchbbq.com/'
-  },
-  {
-    day: 'Friday',
-    date: 'Oct 20th',
-    name: 'Los Dos',
-    url: 'http://www.los2potrillos.com/'
-  },
-  {
-    day: 'Saturday',
-    date: 'Oct 21st',
-    name: 'Arepa’s Caribbean Food',
-    url: ''
-  },
-  {
-    day: 'Sunday',
-    date: 'Oct 22nd',
-    name: 'Tacos Al Sabor',
-    url: ''
-  },
-  {
-    day: 'Monday',
-    date: 'Oct 23rd',
-    name: 'Big Belly BBQ',
-    url: ''
-  },
-  {
-    day: 'Tuesday',
-    date: 'Oct 24th',
-    name: 'Rag Bag Bistro',
-    url: ''
-  },
-  {
-    day: 'Wednesday',
-    date: 'Oct 25th',
-    name: 'Revolver',
-    url: 'https://www.revolverfoodtruck.com/menus'
-  },
-  {
-    day: 'Thursday',
-    date: 'Oct 26th',
-    name: 'Rolling Italian',
-    url: 'https://rollingitalianonline.square.site/'
-  },
-  {
-    day: 'Friday',
-    date: 'Oct 27th',
-    name: 'Seasoned Swine',
-    url: 'https://www.seasonedswine.com/food-truck'
-  },
-  {
-    day: 'Saturday',
-    date: 'Oct 28th',
-    name: 'TBD & Simply Le Crepes',
-    url: ''
-  },
-  {
-    day: 'Sunday',
-    date: 'Oct 29th',
-    name: 'Market 86',
-    url: 'https://www.facebook.com/franktownmarket86/'
-  },
-  {
-    day: 'Monday',
-    date: 'Oct 30th',
-    name: 'TBD',
-    url: ''
-  },
-  {
-    day: 'Tuesday',
-    date: 'Oct 31st',
-    name: 'Ez Eats',
-    url: ''
-  }
-]
+export const data: CalendarItem[] = [{
+  "date": "Oct 1",
+  "name": "Tacos Al Sabor",
+  "day": "Sunday",
+  "url": ""
+},
+{
+  "date": "Oct 2",
+  "name": "Rolling Italian & HipPOPs",
+  "day": "Monday",
+  "url": "https://rollingitalianonline.square.site"
+},
+{
+  "date": "Oct 3",
+  "name": "Revolver",
+  "day": "Tuesday",
+  "url": "https://www.revolverfoodtruck.com/menus"
+},
+{
+  "date": "Oct 4",
+  "name": " Samos Grill",
+  "day": "Wednesday",
+  "url": "https://roaminghunger.com/samos-grill/"
+},
+{
+  "date": "Oct 5",
+  "name": "Woodhill Small Batch BBQ",
+  "day": "Thursday",
+  "url": "http://woodhillsmallbatchbbq.com/"
+},
+{
+  "date": "Oct 6",
+  "name": "Los Dos",
+  "day": "Friday",
+  "url": "http://www.los2potrillos.com/"
+},
+{
+  "date": "Oct 7",
+  "name": "Los Chamacos",
+  "day": "Saturday",
+  "url": "https://www.facebook.com/people/El-chamacos-taco-dealer-my-legacy/100057675699460/"
+},
+{
+  "date": "Oct 8",
+  "name": "No Food Truck",
+  "day": "Sunday",
+  "url": ""
+},
+{
+  "date": "Oct 9",
+  "name": "No Food Truck",
+  "day": "Monday",
+  "url": ""
+},
+{
+  "date": "Oct 10",
+  "name": "Rag Bag BIstro",
+  "day": "Tuesday",
+  "url": ""
+},
+{
+  "date": "Oct 11",
+  "name": "Arepa's Caribbean Food & Simply Le Crepes",
+  "day": "Wednesday",
+  "url": ""
+},
+{
+  "date": "Oct 12",
+  "name": "PinkTank",
+  "day": "Thursday",
+  "url": "https://www.eatpinkgettanked.com/"
+},
+{
+  "date": "Oct 13",
+  "name": "Schnitzelwirts ",
+  "day": "Friday",
+  "url": ""
+},
+{
+  "date": "Oct 14",
+  "name": " Originals by Greeks ",
+  "day": "Saturday",
+  "url": ""
+},
+{
+  "date": "Oct 15",
+  "name": "MIke's Coneys (3pm - 8 pm)",
+  "day": "Sunday",
+  "url": ""
+},
+{
+  "date": "Oct 16",
+  "name": "Seasoned Swine & HipPops",
+  "day": "Monday",
+  "url": ""
+},
+{
+  "date": "Oct 17",
+  "name": "Sizzle",
+  "day": "Tuesday",
+  "url": "https://www.sizzlefoodtruck.com/menu.html"
+},
+{
+  "date": "Oct 18",
+  "name": "Isan Thai",
+  "day": "Wednesday",
+  "url": ""
+},
+{
+  "date": "Oct 19",
+  "name": "Woodhill Small Batch BBQ",
+  "day": "Thursday",
+  "url": "http://woodhillsmallbatchbbq.com/"
+},
+{
+  "date": "Oct 20",
+  "name": "Los Dos",
+  "day": "Friday",
+  "url": "http://www.los2potrillos.com/"
+},
+{
+  "date": "Oct 21",
+  "name": "Arepa's Caribbean Food",
+  "day": "Saturday",
+  "url": ""
+},
+{
+  "date": "Oct 22",
+  "name": "Tacos Al Sabor",
+  "day": "Sunday",
+  "url": ""
+},
+{
+  "date": "Oct 23",
+  "name": "Big Belly BBQ",
+  "day": "Monday",
+  "url": ""
+},
+{
+  "date": "Oct 24",
+  "name": "Rag Bag Bistro",
+  "day": "Tuesday",
+  "url": ""
+},
+{
+  "date": "Oct 25",
+  "name": "Revolver",
+  "day": "Wednesday",
+  "url": "https://www.revolverfoodtruck.com/menus"
+},
+{
+  "date": "Oct 26",
+  "name": "Rolling Italian",
+  "day": "Thursday",
+  "url": "https://rollingitalianonline.square.site/"
+},
+{
+  "date": "Oct 27",
+  "name": "Seasoned Swine",
+  "day": "Friday",
+  "url": "https://www.seasonedswine.com/food-truck"
+},
+{
+  "date": "Oct 28",
+  "name": "TBD & Simply Le Crepes",
+  "day": "Saturday",
+  "url": ""
+},
+{
+  "date": "Oct 29",
+  "name": "Market 86",
+  "day": "Sunday",
+  "url": "https://www.facebook.com/franktownmarket86/"
+},
+{
+  "date": "Oct 30",
+  "name": "TBD",
+  "day": "Monday",
+  "url": ""
+},
+{
+  "date": "Oct 31",
+  "name": "Ez Eats",
+  "day": "Tuesday",
+  "url": ""
+}]; 
+
