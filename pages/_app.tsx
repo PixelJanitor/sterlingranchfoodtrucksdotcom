cc1838ce9b7b634ea081c2b396b40c55794607d4
Derek Briggs <derek.briggs@me.com>
2023-12-21 15:02:38 -0700

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { LazyMotion, domMax } from 'framer-motion'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LazyMotion features={domMax}>
       <Component {...pageProps} />
+      <Analytics />
     </LazyMotion>
   )
 }
