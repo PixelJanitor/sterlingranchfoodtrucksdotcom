10a1af6faa3c5dcfa2f5dc7a038c2a001e52f30a
Tom Charles <thomas.charles23@gmail.com>
2024-05-10 10:15:41 -0600

    add May - July schedule

--- data/index.ts
+++ data/index.ts
@@ -2,183 +2,555 @@ import { CalendarItem } from '@/utils/types'
 
 export const data: CalendarItem[] = [
   {
-    date: 'Jan 31st',
-    name: 'Lucky Bird & HipPOPs',
+    date: 'May 1st',
+    name: 'What Would Cheesus Do & Rebel Cookie Dough',
     day: 'Wednesday',
-    url: 'https://luckybirdco.com/food-truck'
+    url: ''
   },
   {
-    date: 'Feb 1st',
-    name: 'Woodhill Small Batch BBQ',
+    date: 'May 2nd',
+    name: 'Woodhill BBQ',
     day: 'Thursday',
     url: 'http://woodhillsmallbatchbbq.com/'
   },
   {
-    date: 'Feb 2nd',
-    name: 'Seasoned Swine',
+    date: 'May 3rd',
+    name: 'Los Dos Potrillos',
     day: 'Friday',
-    url: 'https://www.seasonedswine.com/food-truck'
+    url: 'http://www.los2potrillos.com/'
   },
   {
-    date: 'Feb 3rd',
-    name: 'Arepas Caribbean Food',
-    day: 'Saturday',
-    url: 'https://gotruckster.com/food-truck/arepas-caribbean-food#men'
-  },
-  {
-    date: 'Feb 4th',
-    name: 'Rolling Italian',
-    day: 'Sunday',
-    url: 'https://rollingitalianonline.square.site/'
-  },
-  {
-    date: 'Feb 5th',
-    name: 'Turkish Chef',
-    day: 'Monday',
-    url: 'https://www.turkishchefonwheels.com/#8'
-  },
-  {
-    date: 'Feb 6th',
-    name: 'Rag Bag Bistro',
-    day: 'Tuesday',
-    url: 'https://roaminghunger.com/ragbag-mobile/'
-  },
-  {
-    date: 'Feb 7th',
-    name: "Pepe's Tortas & Burgers & Dapper Doughnuts",
-    day: 'Wednesday',
-    url: 'https://www.pepestortasandburgers.org/_files/ugd/92bced_54dd38fc6e4a40a0ac2327d2dd27aca1.pdf'
-  },
-  {
-    date: 'Feb 8th',
-    name: "Meyer's Munchies",
-    day: 'Thursday',
-    url: 'https://www.facebook.com/MeyersMunchies/'
-  },
-  {
-    date: 'Feb 9th',
-    name: 'Deja Roux',
-    day: 'Friday',
-    url: 'https://order.toasttab.com/online/deja-roux'
-  },
-  {
-    date: 'Feb 10th',
-    name: 'Gyro Kings',
-    day: 'Saturday',
-    url: 'https://www.thegyrosking.com/edgewater-public-market-menu'
-  },
-  {
-    date: 'Feb 11th',
-    name: "Smokin' Zos",
-    day: 'Sunday',
-    url: ''
-  },
-  {
-    date: 'Feb 12th',
-    name: 'Lucky Bird',
-    day: 'Monday',
-    url: 'https://luckybirdco.com/food-truck'
-  },
-  {
-    date: 'Feb 13th',
-    name: 'Seasoned Swine',
-    day: 'Tuesday',
-    url: 'https://www.seasonedswine.com/food-truck'
-  },
-  {
-    date: 'Feb 14th',
-    name: "Darleen's Dillaz",
-    day: 'Wednesday',
-    url: ''
-  },
-  {
-    date: 'Feb 15th',
-    name: 'Sizzle',
-    day: 'Thursday',
-    url: 'https://www.sizzlefoodtruck.com/menu.html'
-  },
-  {
-    date: 'Feb 16th',
-    name: "Big Belly Bro's BBQ",
-    day: 'Friday',
-    url: 'https://www.bigbellybrothersbbq.com/menu'
-  },
-  {
-    date: 'Feb 17th',
+    date: 'May 4th',
     name: "Tula's Tapas",
     day: 'Saturday',
     url: 'https://www.tulastapas.com'
   },
   {
-    date: 'Feb 18th',
-    name: 'Moontime Crepes',
+    date: 'May 5th',
+    name: 'Los Dos Potrillos',
     day: 'Sunday',
-    url: ''
+    url: 'http://www.los2potrillos.com/'
   },
   {
-    date: 'Feb 19th',
-    name: 'Arepas Caribbean Food',
-    day: 'Monday',
-    url: 'https://gotruckster.com/food-truck/arepas-caribbean-food#men'
-  },
-  {
-    date: 'Feb 20th',
-    name: 'Turkish Chef',
-    day: 'Tuesday',
-    url: 'https://www.turkishchefonwheels.com/#8'
-  },
-  {
-    date: 'Feb 21st',
-    name: 'High Society Pizza & Dapper Doughnuts',
-    day: 'Wednesday',
-    url: 'https://www.highsocietypizza.com/menu'
-  },
-  {
-    date: 'Feb 22nd',
+    date: 'May 6th',
     name: 'Los Chamacos',
-    day: 'Thursday',
+    day: 'Monday',
     url: 'https://www.facebook.com/people/El-chamacos-taco-dealer-my-legacy/100057675699460/'
   },
   {
-    date: 'Feb 23rd',
-    name: 'Pink Tank',
-    day: 'Friday',
-    url: 'https://www.eatpinkgettanked.com/'
-  },
-  {
-    date: 'Feb 24th',
-    name: 'Denver Taco Truck',
-    day: 'Saturday',
+    date: 'May 7th',
+    name: 'Roll It Up Sushi',
+    day: 'Tuesday',
     url: ''
   },
   {
-    date: 'Feb 25th',
-    name: "Big Belly Bro's BBQ 3 pm - 8 pm",
-    day: 'Sunday',
-    url: 'https://www.bigbellybrothersbbq.com/menu'
-  },
-  {
-    date: 'Feb 26th',
-    name: 'Rag Bag Bistro',
-    day: 'Monday',
-    url: 'https://roaminghunger.com/ragbag-mobile/'
-  },
-  {
-    date: 'Feb 27th',
-    name: 'Revolver Food Truck & HipPOPs',
-    day: 'Tuesday',
-    url: 'https://www.revolverfoodtruck.com/menus'
-  },
-  {
-    date: 'Feb 28th',
+    date: 'May 8th',
     name: 'Rolling Italian',
     day: 'Wednesday',
     url: 'https://rollingitalianonline.square.site/'
   },
   {
-    date: 'Feb 29th',
-    name: "Meyer's Munchies",
+    date: 'May 9th',
+    name: "Darleen's Dillaz",
     day: 'Thursday',
-    url: 'https://www.facebook.com/MeyersMunchies/'
+    url: ''
+  },
+  {
+    date: 'May 10th',
+    name: 'Cirque Kitchen',
+    day: 'Friday',
+    url: ''
+  },
+  {
+    date: 'May 11th',
+    name: 'Krazy Thai',
+    day: 'Saturday',
+    url: 'https://www.krazythaifood.com/menu/menu'
+  },
+  {
+    date: 'May 12th',
+    name: 'Super Smash Burgers',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'May 13th',
+    name: 'Sokare',
+    day: 'Monday',
+    url: ''
+  },
+  {
+    date: 'May 14th',
+    name: 'Turkish Chef',
+    day: 'Tuesday',
+    url: 'https://www.turkishchefonwheels.com/#8'
+  },
+  {
+    date: 'May 15th',
+    name: 'High Society Pizza',
+    day: 'Wednesday',
+    url: 'https://www.highsocietypizza.com/menu'
+  },
+  {
+    date: 'May 16th',
+    name: 'Woodhill BBQ',
+    day: 'Thursday',
+    url: 'http://woodhillsmallbatchbbq.com/'
+  },
+  {
+    date: 'May 17th',
+    name: 'Pink Tank',
+    day: 'Friday',
+    url: 'https://www.eatpinkgettanked.com/'
+  },
+  {
+    date: 'May 18th',
+    name: 'Lucky Bird',
+    day: 'Saturday',
+    url: 'https://luckybirdco.com/food-truck'
+  },
+  {
+    date: 'May 19th',
+    name: "Repicci's Real Italian Ice & Gelato (1 pm - 5 pm)",
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'May 20th',
+    name: "Big Belly Bro's BBQ",
+    day: 'Monday',
+    url: 'https://www.bigbellybrothersbbq.com/menu'
+  },
+  {
+    date: 'May 21st',
+    name: 'Roll It Up Sushi',
+    day: 'Tuesday',
+    url: ''
+  },
+  {
+    date: 'May 22nd',
+    name: 'Rolling Italian',
+    day: 'Wednesday',
+    url: 'https://rollingitalianonline.square.site/'
+  },
+  {
+    date: 'May 23rd',
+    name: 'Seasoned Swine',
+    day: 'Thursday',
+    url: 'https://www.seasonedswine.com/food-truck'
+  },
+  {
+    date: 'May 24th',
+    name: 'Moontime Crepes',
+    day: 'Friday',
+    url: ''
+  },
+  {
+    date: 'May 25th',
+    name: 'Gyro Kings',
+    day: 'Saturday',
+    url: 'https://www.thegyrosking.com/edgewater-public-market-menu'
+  },
+  {
+    date: 'May 26th',
+    name: 'Krazy Thai',
+    day: 'Sunday',
+    url: 'https://www.krazythaifood.com/menu/menu'
+  },
+  {
+    date: 'May 27th',
+    name: 'Wheels on Fire Pizza',
+    day: 'Monday',
+    url: ''
+  },
+  {
+    date: 'May 28th',
+    name: "Smokin' Zo's",
+    day: 'Tuesday',
+    url: ''
+  },
+  {
+    date: 'May 29th',
+    name: 'Sokare',
+    day: 'Wednesday',
+    url: ''
+  },
+  {
+    date: 'May 30th',
+    name: 'Samos Grill',
+    day: 'Thursday',
+    url: 'https://roaminghunger.com/samos-grill/'
+  },
+  {
+    date: 'May 31st',
+    name: 'Sizzle',
+    day: 'Friday',
+    url: 'https://www.sizzlefoodtruck.com/menu.html'
+  },
+  {
+    date: 'Jun 1st',
+    name: 'Lucky Bird Co.',
+    day: 'Saturday',
+    url: ''
+  },
+  {
+    date: 'Jun 2nd',
+    name: '',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jun 3rd',
+    name: 'Big Belly Bros BBQ',
+    day: 'Monday',
+    url: 'https://www.bigbellybrothersbbq.com/menu'
+  },
+  {
+    date: 'Jun 4th',
+    name: 'Samos Grill',
+    day: 'Tuesday',
+    url: 'https://roaminghunger.com/samos-grill/'
+  },
+  {
+    date: 'Jun 5th',
+    name: 'Sokare',
+    day: 'Wednesday',
+    url: ''
+  },
+  {
+    date: 'Jun 6th',
+    name: 'Turkish Chef',
+    day: 'Thursday',
+    url: 'https://www.turkishchefonwheels.com/#8'
+  },
+  {
+    date: 'Jun 7th',
+    name: 'Muy Loco Tacos',
+    day: 'Friday',
+    url: ''
+  },
+  {
+    date: 'Jun 8th',
+    name: 'Seasoned Swine',
+    day: 'Saturday',
+    url: 'https://www.seasonedswine.com/food-truck'
+  },
+  {
+    date: 'Jun 9th',
+    name: 'Pizza Box Truck',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jun 10th',
+    name: 'Krazy Thai',
+    day: 'Monday',
+    url: 'https://www.krazythaifood.com/menu/menu'
+  },
+  {
+    date: 'Jun 11th',
+    name: 'Pink Tank',
+    day: 'Tuesday',
+    url: 'https://www.eatpinkgettanked.com/'
+  },
+  {
+    date: 'Jun 12th',
+    name: 'What Would Cheesus Do & Rebel Cookie Dough',
+    day: 'Wednesday',
+    url: ''
+  },
+  {
+    date: 'Jun 13th',
+    name: 'High Society Pizza',
+    day: 'Thursday',
+    url: 'https://www.highsocietypizza.com/menu'
+  },
+  {
+    date: 'Jun 14th',
+    name: 'Los Dos Potrillos',
+    day: 'Friday',
+    url: 'http://www.los2potrillos.com/'
+  },
+  {
+    date: 'Jun 15th',
+    name: 'Crock Spot',
+    day: 'Saturday',
+    url: ''
+  },
+  {
+    date: 'Jun 16th',
+    name: 'Uptown & Humbolt',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jun 17th',
+    name: 'Super Smash Burgers',
+    day: 'Monday',
+    url: ''
+  },
+  {
+    date: 'Jun 18th',
+    name: 'Seasoned Swine, HipPOPs',
+    day: 'Tuesday',
+    url: ''
+  },
+  {
+    date: 'Jun 19th',
+    name: 'Turkish Chef',
+    day: 'Wednesday',
+    url: 'https://www.turkishchefonwheels.com/#8'
+  },
+  {
+    date: 'Jun 20th',
+    name: 'Woodhill Small Batch BBQ',
+    day: 'Thursday',
+    url: 'http://woodhillsmallbatchbbq.com/'
+  },
+  {
+    date: 'Jun 21st',
+    name: 'Los Chamacos',
+    day: 'Friday',
+    url: 'https://www.facebook.com/people/El-chamacos-taco-dealer-my-legacy/100057675699460/'
+  },
+  {
+    date: 'Jun 22nd',
+    name: 'Pepe’s Tortas',
+    day: 'Saturday',
+    url: ''
+  },
+  {
+    date: 'Jun 23rd',
+    name: 'Cirque Kitchen, Kona',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jun 24th',
+    name: 'Krazy Thai',
+    day: 'Monday',
+    url: 'https://www.krazythaifood.com/menu/menu'
+  },
+  {
+    date: 'Jun 25th',
+    name: 'Samos Grill',
+    day: 'Tuesday',
+    url: 'https://roaminghunger.com/samos-grill/'
+  },
+  {
+    date: 'Jun 26th',
+    name: 'Big Belly Bros BBQ',
+    day: 'Wednesday',
+    url: 'https://www.bigbellybrothersbbq.com/menu'
+  },
+  {
+    date: 'Jun 27th',
+    name: 'Rolling Italian',
+    day: 'Thursday',
+    url: 'https://rollingitalianonline.square.site/'
+  },
+  {
+    date: 'Jun 28th',
+    name: 'Los Dos Potrillos',
+    day: 'Friday',
+    url: 'http://www.los2potrillos.com/'
+  },
+  {
+    date: 'Jun 29th',
+    name: 'Sokare',
+    day: 'Saturday',
+    url: ''
+  },
+  {
+    date: 'Jun 30th',
+    name: 'Pizza Box Truck',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jul 1st',
+    name: 'Roll It Up Sushi',
+    day: 'Monday',
+    url: ''
+  },
+  {
+    date: 'Jul 2nd',
+    name: 'Pizza Box Truck',
+    day: 'Tuesday',
+    url: ''
+  },
+  {
+    date: 'Jul 3rd',
+    name: 'Turkish Chef, Kona Ice',
+    day: 'Wednesday',
+    url: ''
+  },
+  {
+    date: 'Jul 4th',
+    name: 'Muy Loco Tacos (1pm- 8 pm)',
+    day: 'Thursday',
+    url: ''
+  },
+  {
+    date: 'Jul 5th',
+    name: 'Los Dos Potrillos',
+    day: 'Friday',
+    url: 'http://www.los2potrillos.com/'
+  },
+  {
+    date: 'Jul 6th',
+    name: '',
+    day: 'Saturday',
+    url: ''
+  },
+  {
+    date: 'Jul 7th',
+    name: 'Wheel on Fire Pizza',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jul 8th',
+    name: 'Rag Bag Bistro',
+    day: 'Monday',
+    url: 'https://roaminghunger.com/ragbag-mobile/'
+  },
+  {
+    date: 'Jul 9th',
+    name: 'Pink Tank',
+    day: 'Tuesday',
+    url: 'https://www.eatpinkgettanked.com/'
+  },
+  {
+    date: 'Jul 10th',
+    name: 'High Society Pizza, Ba-Nom-a-Nom',
+    day: 'Wednesday',
+    url: ''
+  },
+  {
+    date: 'Jul 11th',
+    name: 'Deja Roux',
+    day: 'Thursday',
+    url: 'https://order.toasttab.com/online/deja-roux'
+  },
+  {
+    date: 'Jul 12th',
+    name: 'Rolling Italian',
+    day: 'Friday',
+    url: 'https://rollingitalianonline.square.site/'
+  },
+  {
+    date: 'Jul 13th',
+    name: 'Uptown & Humboldt',
+    day: 'Saturday',
+    url: ''
+  },
+  {
+    date: 'Jul 14th',
+    name: 'Gyros King',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jul 15th',
+    name: 'Big Belly BBQ',
+    day: 'Monday',
+    url: ''
+  },
+  {
+    date: 'Jul 16th',
+    name: 'Cirque Kitchen',
+    day: 'Tuesday',
+    url: ''
+  },
+  {
+    date: 'Jul 17th',
+    name: 'Cruz In & Eat, HipPOPs',
+    day: 'Wednesday',
+    url: ''
+  },
+  {
+    date: 'Jul 18th',
+    name: 'Woodhill Small Batch BBQ',
+    day: 'Thursday',
+    url: 'http://woodhillsmallbatchbbq.com/'
+  },
+  {
+    date: 'Jul 19th',
+    name: 'Los Dos Potrillos',
+    day: 'Friday',
+    url: 'http://www.los2potrillos.com/'
+  },
+  {
+    date: 'Jul 20th',
+    name: 'Lucky Bird',
+    day: 'Saturday',
+    url: 'https://luckybirdco.com/food-truck'
+  },
+  {
+    date: 'Jul 21st',
+    name: 'Uptown & Humboldt',
+    day: 'Sunday',
+    url: ''
+  },
+  {
+    date: 'Jul 22nd',
+    name: 'El Chamacos',
+    day: 'Monday',
+    url: ''
+  },
+  {
+    date: 'Jul 23rd',
+    name: 'Sizzle',
+    day: 'Tuesday',
+    url: 'https://www.sizzlefoodtruck.com/menu.html'
+  },
+  {
+    date: 'Jul 24th',
+    name: 'Rolling Italian, Happy Cones',
+    day: 'Wednesday',
+    url: ''
+  },
+  {
+    date: 'Jul 25th',
+    name: 'Chibby Wibbitz',
+    day: 'Thursday',
+    url: ''
+  },
+  {
+    date: 'Jul 26th',
+    name: 'J Street',
+    day: 'Friday',
+    url: ''
+  },
+  {
+    date: 'Jul 27th',
+    name: 'Street Side Eats',
+    day: 'Saturday',
+    url: ''
+  },
+  {
+    date: 'Jul 28th',
+    name: 'Rag Bag Bistro',
+    day: 'Sunday',
+    url: 'https://roaminghunger.com/ragbag-mobile/'
+  },
+  {
+    date: 'Jul 29th',
+    name: 'Cirque Kitchen',
+    day: 'Monday',
+    url: ''
+  },
+  {
+    date: 'Jul 30th',
+    name: 'Samos Grill',
+    day: 'Tuesday',
+    url: 'https://roaminghunger.com/samos-grill/'
+  },
+  {
+    date: 'Jul 31st',
+    name: 'What Would Cheesus Do, Rebel Cookie Dough & Confections',
+    day: 'Wednesday',
+    url: ''
   }
 ]
